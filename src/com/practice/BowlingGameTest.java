58540ed4b24c1161025f0df3d2d287368bb532e3
Vianney MAYNADIER <lenovo_21@DESKTOP-DB4UJ4D>
2019-03-01 16:19:36 +0100

    premier test - création d'une partie

--- src/com/practice/BowlingGameTest.java
+++ src/com/practice/BowlingGameTest.java
@@ -1,7 +1,22 @@
 package com.practice;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BowlingGameTest {
+
+    private BowlingGame object;
+
+    @Before
+    public void initGame() {
+        object = new BowlingGame();
+    }
+
+    @Test
+    public void test() {
+        assertNotNull("BowlingGame ne peut être null", object);
+    }
 
 }
